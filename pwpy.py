c1811fde382befdd7f05826d7ea7d2ddeee5a6a4
Bachiri Mohamed Sifeddine <62722469+medsifeddine16@users.noreply.github.com>
2020-05-16 00:48:28 +0100

    Update pwpy.py

--- pwpy.py
+++ pwpy.py
@@ -17,6 +17,7 @@ name = str(input('> Name:'))
 nickname = str(input('> Nickname:'))
 surname = str(input('> Surname:'))
 birthday = str(input("> Birthdate (DD/MM/YYYY):"))
+
 try:
     d, m, y = birthday.split('/')
 except ValueError:
@@ -29,29 +30,42 @@ print("\n")
 best_friend = str(input('> Girlfriend Name:'))
 best_friend_nickname = str(input('> Girlfriend Nickname:'))
 best_friend_birthdate = str(input('> Girlfriend Birthdate (DD/MM/YYYY):'))
+
 try:
     d1, m1, y1 = best_friend_birthdate.split('/')
+
 except ValueError:
     d1 = ''
     m1 = ''
     y1 = ''
 
 print("\n")
+
 phone_number = str(input('> Phone Number:'))
 favorite_team = str(input('> Favorite Team:'))
 
 words = [name, nickname, surname, d, m, y, best_friend, d1, m1, y1, best_friend_nickname, phone_number, favorite_team]
+
+while("" in words):
+   words.remove("")
+
 password_list = []
+
 print('\n')
+
 xyz = input("Do you want to add some key words about the victim? Y/[N]:")
+
 if xyz == 'y' or xyz == 'Y':
     password_list = words + key_words
 else:
     password_list = words
-min_length, max_length = 1, 3
+
+min_length, max_length = 1, 4
+
 for n in range(min_length, max_length+1):
     for xs in itertools.product(password_list, repeat=n):
         print(''.join(xs), file=open("passwords.txt", "a"))
+
 print("\n")
 print('[+] Now making a dictionary...')
 print("[+] Sorting list and removing duplicates...")
